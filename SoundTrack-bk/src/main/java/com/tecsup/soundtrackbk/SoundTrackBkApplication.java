3a153810d5b477414a3fa4b375be92329b4664c6
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-07-09 13:21:51 -0500

    Repository Initialization

---
+++ SoundTrack-bk/src/main/java/com/tecsup/soundtrackbk/SoundTrackBkApplication.java
@@ -0,0 +1,13 @@
+package com.tecsup.soundtrackbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundTrackBkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundTrackBkApplication.class, args);
+    }
+
+}
---
+++ SoundTrack-bk/src/test/java/com/tecsup/soundtrackbk/SoundTrackBkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecsup.soundtrackbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundTrackBkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
